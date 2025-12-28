5a322cdc72407088af6f5e0adf08be32a5be70d4
Vitmalok <ivanov@uchitelnik.ru>
2025-12-28 06:47:40 +0300

    introduce cmake

--- build.sh
+++ build.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-#g++ "main.cpp" "MapObject.cpp" "Player.cpp" "Tile.cpp" "World.cpp" -o "main"
 
-g++ "src/main.cpp" -c -o "build/main.o"
-g++ "src/TextureManager.cpp" -c -o "build/TextureManager.o"
-g++ "src/Utils.cpp" -c -o "build/Utils.o"
-g++ "src/MapObject.cpp" -c -o "build/MapObject.o"
-g++ "src/Character.cpp" -c -o "build/Character.o"
-g++ "src/Player.cpp" -c -o "build/Player.o"
-g++ "src/Tile.cpp" -c -o "build/Tile.o"
-g++ "src/World.cpp" -c -o "build/World.o"
-
-g++ "build/main.o" "build/TextureManager.o" "build/Utils.o" "build/MapObject.o" "build/Character.o" "build/Player.o" "build/Tile.o" "build/World.o" -o "main" -lsfml-graphics -lsfml-window -lsfml-system 
+cmake -S . -B build -G Ninja
+ninja -C build
